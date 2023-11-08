9da760a6eefcbff6863745de7c546d4630010e90
Joseph Rydel <jrydel92@gmail.com>
2023-11-07 22:15:23 -0600

    Exception-handling and user validation added.

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,14 +1,21 @@
 import pandas
-
-def get_word(my_dict, entry):
-    my_word = entry.upper()
-    my_list = [my_dict[letter] for letter in my_word]
+def check_input(my_dict):
+    my_list = []
+    word = input("Enter a word: ").upper()
+    try:
+        my_list = [my_dict[letter] for letter in word]
+    except KeyError:
+        correct = False
+        while not correct:
+            print("Only letters can be accepted. Try again.")
+            word = input("Enter a word: ").upper()
+            if word.isalpha():
+                correct = True
+                my_list = [my_dict[letter] for letter in word]
     return  my_list
 
 letter_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 letter_dict = letter_data.set_index("letter")["code"].to_dict()
 
-
-word = input("Enter a word: ")
-word_list = get_word(letter_dict, word)
+word_list = check_input(letter_dict)
 print(word_list)
